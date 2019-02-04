f7f40f1f9396b16e1d19830a330db0d501b01f4a
Shaun Thornburgh <personal@shaunthornburgh.com>
2019-02-04 22:21:11 +0000

    JWT Authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://shaunthornburgh:q5vWGktASW8bSur7p6dA2XJBarJd5hV8@ds159634.mlab.com:59634/devconnector2'
+    mongoURI: 'mongodb://shaunthornburgh:q5vWGktASW8bSur7p6dA2XJBarJd5hV8@ds159634.mlab.com:59634/devconnector2',
+    secretOrKey: 'dfjuihgw%46856309ruf&Y*^TGUYurg'
 };
