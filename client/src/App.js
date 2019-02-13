40985737891a28ec1e0316100f7570a9093f72e1
Shaun Thornburgh <personal@shaunthornburgh.com>
2019-02-13 19:42:14 +0000

    Install theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/theme.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css'
+import * as $ from 'jquery/jquery.min.js';
+import 'in-view/dist/in-view.min.js';
+import './assets/js/theme.min.js';
+import Header from './components/layout/Header';
+import BookLover from './assets/img/svg/book-lover.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <main>
+          <section className="section-rotate">
+              <div className="section-inner bg-gradient-primary"></div>
+              <div className="container pt-lg">
+                  <div className="row">
+                      <div className="col-lg-5">
+                          <div className="pt-lg-md text-center text-lg-left">
+                              <h1 className="text-white mb-0 lh-150">
+                          This is the new era of
+                          <span className="text-white font-weight-bold">web development</span>.
+                      </h1>
+                              <p className="lead text-white lh-180">Build responsive, mobile-first projects on the web with the world's most popular front-end component library.</p>
+                              <div className="mt-5">
+                                  <a href="#" className="btn btn-info btn-circle btn-translate--hover mr-4">Get started</a>
+                                  <a href="#" className="btn btn-outline-white btn-circle btn-translate--hover">See how it works</a>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="col-lg-7">
+                          <img alt="Image placeholder" src={BookLover} className="img-fluid d-none d-lg-block" />
+                      </div>
+                  </div>
+              </div>
+          </section>
+        </main>
       </div>
     );
   }
