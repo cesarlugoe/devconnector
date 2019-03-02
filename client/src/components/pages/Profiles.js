80493c13bcd9f541c7634dc5c83213fa82a1dd10
Cesar Lugo <cesarlugo00@hotmail.com>
2019-03-02 13:19:57 +0100

    donde

--- client/src/components/pages/Profiles.js
+++ client/src/components/pages/Profiles.js
@@ -1,10 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Segment, Button, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 export default class Profiles extends Component {
   render() {
     return (
-      <div>
-        profiles
+      <div className="explore-profiles-container">
+        <div className="explore-profiles">
+            <h2>Developer Profiles</h2>
+            <p>Browse and connect with developers</p>
+            {/* this is a profile card, when having database use map function and use component to render for each developer in DB */}
+              <div className="explore-profile-card">
+                <div className="explore-profile-card-left-section">
+                  <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200" alt=""></img>
+                  <div>
+                    <h2>John Doe</h2>
+                    <p>Developer at Microsoft</p>
+                    <p>Seattle, WA</p>
+                    <Link to="/profile"><Button className="primary">View Profile</Button></Link>
+                  </div>
+                </div>
+                <div className="explore-profile-card-right-section">
+                  <h3>Skill Set</h3>
+                  <div>
+                    <Segment.Group piled className="profile-card-segment">
+                      <Segment textAlign='left'><Icon className="check"></Icon>HTML</Segment>
+                      <Segment textAlign='left'><Icon className="check"></Icon>CSS</Segment>
+                      <Segment textAlign='left'><Icon className="check"></Icon>JavaScript</Segment>
+                      <Segment textAlign='left'><Icon className="check"></Icon>Python</Segment>
+                      <Segment textAlign='left'><Icon className="check"></Icon>C#</Segment>
+                    </Segment.Group>
+                  </div>
+                </div>
+              </div>
+              {/* ------------------------End of 1 developer card Profile ------------------------------------- */}
+              <div className="explore-profile-card">
+                <div className="explore-profile-card-left-section">
+                  <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200" alt=""></img>
+                  <div>
+                    <h2>John Doe</h2>
+                    <p>Developer at Microsoft</p>
+                    <p>Seattle, WA</p>
+                    <Link to="/profile"><Button className="primary">View Profile</Button></Link>
+                  </div>
+                </div>
+                <div className="explore-profile-card-right-section">
+                  <h3>Skill Set</h3>
+                  <div>
+                    <Segment.Group piled className="profile-card-segment">
+                      <Segment textAlign='left'><Icon className="check"></Icon>HTML</Segment>
+                      <Segment textAlign='left'><Icon className="check"></Icon>CSS</Segment>
+                      <Segment textAlign='left'><Icon className="check"></Icon>JavaScript</Segment>
+                      <Segment textAlign='left'><Icon className="check"></Icon>Python</Segment>
+                      <Segment textAlign='left'><Icon className="check"></Icon>C#</Segment>
+                    </Segment.Group>
+                  </div>
+                </div>
+              </div>
+        </div>
       </div>
     )
   }
--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -1,10 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Form, Button } from 'semantic-ui-react';
 
 export default class Register extends Component {
   render() {
     return (
-      <div>
-        register
+      <div className="register-container">
+        <div className="register">
+            <div className="register-title-container">
+              <h1> Sign-up </h1>
+              <p>Create your DevConnector account</p>
+            </div>
+            <Form>
+                <Form.Field>
+                  <input placeholder="Name"/>
+                </Form.Field>
+                <Form.Field>
+                  <input placeholder="Email Address"/>
+                  <label>This site uses Gravatar so if you want a profile image, use a Gravatar email</label>
+                </Form.Field>
+                <Form.Field>
+                  <input placeholder="Password"/>
+                </Form.Field>
+                <Form.Field>
+                  <input placeholder="Confirm Password"/>
+                </Form.Field>
+                <Button className="primary" fluid>Submit</Button>
+            </Form>
+          </div>
       </div>
     )
   }
